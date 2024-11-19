11042c5bda03862a0c7242859ae3f947f872864e
PoriaSherafatian <poriasherafatian@gmail.com>
2024-11-19 12:03:51 +0330

    Create GitHub repo for insert code to it make collab to other person who can handle front-end project

---
+++ src/test/java/dev/Poria/collabProject/CollabProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.Poria.collabProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
